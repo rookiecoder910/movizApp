872554f0c5961f4d9cb0f4e044685302c3dae07c
rookiecoder910 <carnageinside192@gmail.com>
2025-10-16 00:49:37 +0530

    added view movie navigation now the user can click on movie card to see all the details of movie

---
+++ app/src/main/java/com/example/movizapp/navigation/NavGraph.kt
@@ -0,0 +1,31 @@
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.movizapp.screens.MovieScreen
+import com.example.movizapp.viewmodel.MovieViewModel
+
+@Composable
+fun MovizNavGraph(
+    viewModel: MovieViewModel,
+    navController: NavHostController
+) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+         MovieScreen(viewModel = viewModel, navController = navController)
+        }
+
+        composable(
+            "movieDetail/{movieId}",
+            arguments = listOf(navArgument("movieId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val movieId = backStackEntry.arguments?.getInt("movieId") ?: 0
+            MovieDetailScreen(movieId = movieId, viewModel = viewModel, navController = navController)
+        }
+    }
+}
--- app/src/main/java/com/example/movizapp/screens/MovieDetailScreen.kt
+++ app/src/main/java/com/example/movizapp/screens/MovieDetailScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+
 import coil.compose.AsyncImage
 import com.example.movizapp.viewmodel.MovieViewModel
 
